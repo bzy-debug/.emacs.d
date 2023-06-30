e7da9559bab7268159bb376b6c43b5f791bdd114
Bao Zhiyuan <baozhiyuan@idea.edu.cn>
2023-06-30 08:57:02 +0800

    OCaml replated stuff

--- init.el
+++ init.el
@@ -80,6 +80,8 @@
 ;; Latex
 ;; (require 'init-auctex)
 
+(require 'dune)
+
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup)
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
