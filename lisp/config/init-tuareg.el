b97eb9961ee7c6e2e464f86a5e99c793f4436778
Bao Zhiyuan <baozhiyuan@idea.edu.cn>
2023-07-02 15:08:21 +0800

    Setup ocamlformat

--- lisp/config/init-tuareg.el
+++ lisp/config/init-tuareg.el
@@ -1,4 +1,11 @@
 (require 'tuareg)
+(require 'ocamlformat)
+
+(defun my-tuareg-mode-hook ()
+  (keymap-set tuareg-mode-map "C-M-<tab>" #'ocamlformat)
+  (ocamlformat-setup-indent)
+  (ocamlformat-set-newline-and-indent)
+  (add-hook 'before-save-hook #'ocamlformat-before-save))
 
 (add-to-list 'auto-mode-alist '("\\.ml[iylp]?$" . tuareg-mode))
 (add-to-list 'auto-mode-alist '("\\.topml$" . tuareg-mode))
